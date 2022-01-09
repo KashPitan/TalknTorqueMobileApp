15d264b9bb8d30978949bbe3b7f58bcdfcc6ff04
Kash Pitan <kashpitan@googlemail.com>
2022-01-09 15:23:45 +0000

    updated eventCard to use files from firebase storage

--- src/components/EventCard.tsx
+++ src/components/EventCard.tsx
@@ -1,48 +1,66 @@
-import React from "react";
+import React, { FC, useEffect, useState } from "react";
 import { StyleSheet, ImageBackground } from "react-native";
 import { Box, Button, Text, VStack, Image, Center } from "native-base";
 import TTPic from "../../assets/images/TTPic.png";
 import EventCardDate from "./EventCardDate";
+import { storage } from "../../firebase";
+import { getDownloadURL, ref } from "firebase/storage";
+import { EventType } from "../../types";
+const EventCard: FC<{ event: EventType }> = ({ event }): JSX.Element => {
+  const firebaseStorageReference = ref(storage, event.imageUri);
+  const [downloadUrl, setDownloadUrl] = useState<string | null>(null);
 
-const EventCard = () => {
+  useEffect(() => {
+    (async () => {
+      const firebaseDownloadUrl = await getDownloadURL(
+        firebaseStorageReference
+      );
+      setDownloadUrl(firebaseDownloadUrl);
+      console.log(firebaseDownloadUrl);
+    })();
+  }, []);
   return (
     <Box rounded="2xl" bg="gray.200" w="90%" h="67%">
       <VStack alignItems="center">
-        <ImageBackground
-          source={TTPic}
-          resizeMode="cover"
-          style={styles.image}
-          borderRadius={20}
-        >
-          <Center
-            bg="gray.100"
-            position="absolute"
-            top="270"
-            left="0"
-            px="3"
-            py="0.5"
-            roundedBottomLeft="xl"
-            borderWidth="2"
-            borderColor="gray.300"
+        {downloadUrl && (
+          <ImageBackground
+            source={{ uri: downloadUrl }}
+            resizeMode="cover"
+            style={styles.image}
+            borderRadius={20}
           >
-            <VStack>
-              <Text bold fontSize="sm">
-                We Love Cars
-              </Text>
-            </VStack>
-          </Center>
+            <Center
+              bg="gray.100"
+              position="absolute"
+              top="270"
+              left="0"
+              px="3"
+              py="0.5"
+              roundedBottomLeft="xl"
+              borderWidth="2"
+              borderColor="gray.300"
+            >
+              <VStack>
+                <Text bold fontSize="sm">
+                  We Love Cars
+                </Text>
+              </VStack>
+            </Center>
 
-          <EventCardDate />
-        </ImageBackground>
+            <EventCardDate />
+          </ImageBackground>
+        )}
 
-        {/* <Image
-          roundedTop="2xl"
-          source={TTPic}
-          alt="T&T"
-          size="xl"
-          h="100%"
-          width="full"
-        /> */}
+        {/* {downloadUrl && (
+          <Image
+            roundedTop="2xl"
+            source={{ uri: downloadUrl }}
+            alt="T&T"
+            size="xl"
+            h="100%"
+            width="full"
+          />
+        )} */}
       </VStack>
     </Box>
   );
