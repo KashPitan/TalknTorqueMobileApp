ef3b8af17b22de58c942156535af82b532ed1f52
Kash Pitan <kashpitan@googlemail.com>
2022-03-09 01:02:18 +0000

    new signin background with blur, error on invalid logins

--- src/screens/SignInScreen.tsx
+++ src/screens/SignInScreen.tsx
@@ -1,10 +1,29 @@
-import { useState } from "react";
-import { StyleSheet, Image as rnImage, Keyboard } from "react-native";
-import { Input, Center, Button, Box, Image, Text } from "native-base";
+import { useState, useEffect } from "react";
+import {
+  StyleSheet,
+  Keyboard,
+  ImageBackground,
+  Dimensions,
+} from "react-native";
+import {
+  Input,
+  Center,
+  Button,
+  Box,
+  Image,
+  Text,
+  useToast,
+  FormControl,
+  Icon,
+} from "native-base";
 import { auth } from "../../firebase";
 import { TTLOGO } from "../../assets/index";
 import tandtlogo from "../../images/T&T2022Logo.png";
+import TTBackground from "../../assets/images/TTLoginBackground.png";
 import React from "react";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { BlurView } from "expo-blur";
+
 // const logo = require("../../images/T&T2022Logo.png");
 
 import { signInWithEmailAndPassword } from "firebase/auth";
@@ -15,6 +34,9 @@ const SignInScreen = ({ navigation }) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<null | string>(null);
+
+  const [formIsFocused, setformIsFocused] = useState<boolean>(false);
 
   const buttonWidth = "85%";
   const buttonHeight = "16";
@@ -39,82 +61,155 @@ const SignInScreen = ({ navigation }) => {
       // console.log(error);
       // console.log(error?.message);
       console.log(error.code);
+      if (error.code === "auth/invalid-email") {
+        setError("Incorrect login details");
+      }
       setLoading(false);
     }
   };
 
+  const signInFormOnFocusHandler = () => {
+    setformIsFocused(true);
+  };
+
   return (
-    <Box
-      w="100%"
-      h="100%"
-      bg={{
-        linearGradient: {
-          colors: ["red.400", "red.600"],
-          start: [0, 1],
-          end: [1, 0],
-        },
-      }}
+    <ImageBackground
+      source={TTBackground}
+      resizeMode="stretch"
+      style={styles.backgroundImage}
+      blurRadius={formIsFocused ? 20 : 0}
     >
-      <Center padding="8" flex={1}>
-        <Image source={tandtlogo} alt="T&T" size="2xl" width="full" />
+      <Center padding="8" w="full" h="full" ml="3.5">
+        <FormControl w="full" isInvalid={error}>
+          <FormControl.Label>
+            <Text bold color="black">
+              Email
+            </Text>
+          </FormControl.Label>
+          <Input
+            mb="4"
+            placeholderTextColor="gray.400"
+            placeholder="Login Email"
+            onChangeText={(email) => {
+              setEmail(email);
+              setError(null);
+            }}
+            size="lg"
+            color="black"
+            bgColor="gray.100"
+            w={inputFieldWidth}
+            onFocus={signInFormOnFocusHandler}
+            shadow={5}
+            InputLeftElement={
+              <Icon
+                as={<MaterialCommunityIcons name="email" />}
+                size={5}
+                ml="2"
+                color="muted.400"
+              />
+            }
+          />
+          <FormControl.Label>
+            <Text bold color="black">
+              Password
+            </Text>
+          </FormControl.Label>
+          <Input
+            placeholderTextColor="gray.400"
+            placeholder="Password"
+            type="password"
+            onChangeText={(password) => {
+              setPassword(password);
+              setError(null);
+            }}
+            size="lg"
+            color="white"
+            bgColor="gray.100"
+            w={inputFieldWidth}
+            onFocus={signInFormOnFocusHandler}
+            shadow={5}
+            InputLeftElement={
+              <Icon
+                as={<MaterialCommunityIcons name="lock" />}
+                size={5}
+                ml="2"
+                color="muted.400"
+              />
+            }
+          />
+          <FormControl.ErrorMessage
+            leftIcon={
+              <Icon
+                as={<MaterialCommunityIcons name="alert-circle" />}
+                size={5}
+              />
+            }
+          >
+            <Text bold color="red.600">
+              {error}
+            </Text>
+          </FormControl.ErrorMessage>
+          {loading ? (
+            <Button
+              mt="10"
+              w={buttonWidth}
+              h={buttonHeight}
+              bg="black"
+              onPress={() => handleSignIn()}
+              isLoading
+              isLoadingText="Logging in"
+            >
+              Login
+            </Button>
+          ) : (
+            <Button
+              mt="10"
+              w={buttonWidth}
+              h={buttonHeight}
+              bg="black"
+              style={styles.dropShadow}
+              onPress={() => handleSignIn()}
+            >
+              Login
+            </Button>
+          )}
 
-        <Input
-          m="3"
-          placeholder="enter account email address"
-          placeholderTextColor="white"
-          onChangeText={(email) => setEmail(email)}
-          size="lg"
-          color="white"
-          w={inputFieldWidth}
-        />
-
-        <Input
-          placeholder="enter password"
-          placeholderTextColor="white"
-          type="password"
-          onChangeText={(password) => setPassword(password)}
-          size="lg"
-          color="white"
-          w={inputFieldWidth}
-        />
-        {loading ? (
           <Button
-            mt="10"
             w={buttonWidth}
             h={buttonHeight}
-            bg="black"
-            onPress={() => handleSignIn()}
-            isLoading
-            isLoadingText="Logging in"
+            my="3"
+            bg="white"
+            // borderWidth={1}
+            // borderColor="black"
+            style={styles.dropShadow}
+            onPress={() => navigation.navigate("Register Screen")}
           >
-            Login
+            Register
           </Button>
-        ) : (
-          <Button
-            mt="10"
-            w={buttonWidth}
-            h={buttonHeight}
-            bg="black"
-            onPress={() => handleSignIn()}
-          >
-            Login
-          </Button>
-        )}
-
-        <Button
-          w={buttonWidth}
-          h={buttonHeight}
-          my="3"
-          bg="white"
-          onPress={() => navigation.navigate("Register Screen")}
-        >
-          Register
-        </Button>
+        </FormControl>
       </Center>
-    </Box>
+    </ImageBackground>
   );
 };
 
 export default SignInScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  backgroundImage: {
+    position: "absolute",
+    left: 0,
+    top: 0,
+    width: Dimensions.get("window").width + 20,
+    height: Dimensions.get("window").height + 20,
+  },
+  dropShadow: {
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 20,
+  },
+});
