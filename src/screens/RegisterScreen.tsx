2fade730c8b355cb611c78dbb722561246ab670c
Kash Pitan <kashpitan@googlemail.com>
2022-03-15 00:25:12 +0000

    updated register screen visuals and added form validation

--- src/screens/RegisterScreen.tsx
+++ src/screens/RegisterScreen.tsx
@@ -1,25 +1,48 @@
 import { useState } from "react";
-import { StyleSheet } from "react-native";
-import { Input, Center, Button, Box, Text, useToast } from "native-base";
+import { StyleSheet, ImageBackground, Dimensions } from "react-native";
+import {
+  Input,
+  Center,
+  Button,
+  Text,
+  useToast,
+  FormControl,
+  Icon,
+} from "native-base";
 import React from "react";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+
+import validator from "validator";
 
 import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
 import { auth } from "../../firebase";
 
+import TTBackground from "../../assets/images/TTLoginBackground.png";
+
 const RegisterScreen = ({ navigation }) => {
   const toast = useToast();
+
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [displayName, setDisplayName] = useState("");
   const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<null | string>(null);
 
   const buttonWidth = "85%";
   const buttonHeight = "16";
   const inputFieldWidth = "85%";
 
   const handleRegister = async () => {
-    setLoading(true);
+    validatePassword();
+    validateEmail();
+    validateName();
+    checkFormFilledOut();
+
+    if (error) return;
+
     try {
+      setLoading(true);
+
       const userCredential = await createUserWithEmailAndPassword(
         auth,
         email,
@@ -40,80 +63,190 @@ const RegisterScreen = ({ navigation }) => {
     }
   };
 
+  const checkFormFilledOut = () => {
+    if (!displayName) setError("You must enter a name");
+    if (!password) setError("You must enter a password");
+    if (!email) setError("You must enter an email");
+  };
+
+  const validateName = () => {
+    const isValid = validator.isAlpha(displayName);
+    if (!isValid) setError("Name must only contain letters");
+  };
+
+  const validateEmail = () => {
+    const isValid = validator.isEmail(email);
+    if (!isValid) setError("Email is not valid");
+  };
+
+  const validatePassword = () => {
+    const isValid = validator.isStrongPassword(password, {
+      minLength: 8,
+      minLowercase: 1,
+      minUppercase: 1,
+      minNumbers: 1,
+    });
+    if (!isValid) setError("Password is not strong enough");
+    console.log(isValid);
+  };
+
   return (
-    <Box
-      w="100%"
-      h="100%"
-      padding="10"
-      bg={{
-        linearGradient: {
-          colors: ["red.400", "red.600"],
-          start: [0, 1],
-          end: [1, 0],
-        },
-      }}
+    <ImageBackground
+      source={TTBackground}
+      resizeMode="stretch"
+      style={styles.backgroundImage}
+      blurRadius={20}
     >
-      <Center flex={1}>
-        <Text fontSize="3xl" color="white" bold>
-          Create New Account
-        </Text>
-        <Input
-          m="3"
-          mt="10"
-          placeholder="enter your email address"
-          placeholderTextColor="white"
-          onChangeText={(email) => setEmail(email)}
-          size="lg"
-          w={inputFieldWidth}
-        />
-
-        <Input
-          mb="3"
-          placeholder="enter your name"
-          placeholderTextColor="white"
-          onChangeText={(displayName) => setDisplayName(displayName)}
-          size="lg"
-          w={inputFieldWidth}
-        />
-
-        <Input
-          type="password"
-          placeholder="enter a password"
-          placeholderTextColor="white"
-          onChangeText={(password) => setPassword(password)}
-          size="lg"
-          w={inputFieldWidth}
-        />
-        {loading ? (
-          <Button
-            w={buttonWidth}
-            h={buttonHeight}
-            my="10"
-            bg="white"
+      <Center padding="8" w="full" h="full" ml="3.5">
+        <FormControl w="full" isInvalid={error}>
+          <Text fontSize="3xl" color="black" bold>
+            Create New Account
+          </Text>
+          <FormControl.Label>
+            <Text bold color="black">
+              Email
+            </Text>
+          </FormControl.Label>
+          <Input
+            mb="4"
+            placeholder="enter your email address"
+            placeholderTextColor="gray.400"
+            color="black"
+            bgColor="gray.100"
+            onChangeText={(email) => {
+              setEmail(email);
+              setError(null);
+            }}
             size="lg"
-            onPress={() => handleRegister()}
-            isLoading
-            isLoadingText="Registering"
-          >
-            Register
-          </Button>
-        ) : (
-          <Button
-            w={buttonWidth}
-            h={buttonHeight}
-            my="10"
-            bg="white"
+            w={inputFieldWidth}
+            shadow={5}
+            InputLeftElement={
+              <Icon
+                as={<MaterialCommunityIcons name="email" />}
+                size={5}
+                ml="2"
+                color="muted.400"
+              />
+            }
+          />
+
+          <FormControl.Label>
+            <Text bold color="black">
+              Name
+            </Text>
+          </FormControl.Label>
+          <Input
+            mb="4"
+            placeholder="enter your name"
+            placeholderTextColor="gray.400"
+            color="black"
+            bgColor="gray.100"
+            onChangeText={(displayName) => {
+              setDisplayName(displayName);
+              setError(null);
+            }}
             size="lg"
-            onPress={() => handleRegister()}
+            w={inputFieldWidth}
+            shadow={5}
+            InputLeftElement={
+              <Icon
+                as={<MaterialCommunityIcons name="account" />}
+                size={5}
+                ml="2"
+                color="muted.400"
+              />
+            }
+          />
+
+          <FormControl.Label>
+            <Text bold color="black">
+              Password
+            </Text>
+          </FormControl.Label>
+          <Input
+            type="password"
+            placeholder="enter a password"
+            placeholderTextColor="gray.400"
+            color="black"
+            bgColor="gray.100"
+            onChangeText={(password) => {
+              setPassword(password);
+              setError(null);
+            }}
+            size="lg"
+            w={inputFieldWidth}
+            shadow={5}
+            InputLeftElement={
+              <Icon
+                as={<MaterialCommunityIcons name="lock" />}
+                size={5}
+                ml="2"
+                color="muted.400"
+              />
+            }
+          />
+          <FormControl.ErrorMessage
+            leftIcon={
+              <Icon
+                as={<MaterialCommunityIcons name="alert-circle" />}
+                size={5}
+              />
+            }
           >
-            Register
-          </Button>
-        )}
+            <Text bold color="red.600">
+              {error}
+            </Text>
+          </FormControl.ErrorMessage>
+          {loading ? (
+            <Button
+              w={buttonWidth}
+              h={buttonHeight}
+              my="10"
+              bg="white"
+              size="lg"
+              onPress={() => handleRegister()}
+              isLoading
+              isLoadingText="Registering"
+            >
+              Register
+            </Button>
+          ) : (
+            <Button
+              w={buttonWidth}
+              h={buttonHeight}
+              my="10"
+              bg="white"
+              size="lg"
+              style={styles.dropShadow}
+              onPress={() => handleRegister()}
+            >
+              Register
+            </Button>
+          )}
+        </FormControl>
       </Center>
-    </Box>
+    </ImageBackground>
   );
 };
 
 export default RegisterScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  backgroundImage: {
+    position: "absolute",
+    left: 0,
+    top: 0,
+    width: Dimensions.get("window").width + 20,
+    height: Dimensions.get("window").height + 20,
+  },
+  dropShadow: {
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 20,
+  },
+});
