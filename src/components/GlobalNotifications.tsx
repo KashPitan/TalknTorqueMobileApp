92d9dec2d059cc7ed659d9e0fd900b7b0f54c53b
Kash Pitan <kashpitan@googlemail.com>
2022-05-03 01:41:48 +0100

    set up initial calculations for reminder notifications

--- src/components/GlobalNotifications.tsx
+++ src/components/GlobalNotifications.tsx
@@ -15,6 +15,7 @@ import { db, auth } from "../../firebase";
 
 const GlobalNotifications = () => {
   useEffect(() => {
+    let eventListener;
     const authListener = auth.onAuthStateChanged((user) => {
       if (user) {
         (async () => {
@@ -28,13 +29,40 @@ const GlobalNotifications = () => {
             where("createdAt", ">", nowTimestamp)
           );
           // make this event listener app wide (app.tsx)
-          const eventListener = onSnapshot(newEventQuery, (snapshot) => {
+          eventListener = onSnapshot(newEventQuery, (snapshot) => {
             snapshot.docChanges().forEach((change) => {
               console.log(change.type);
               if (change.type === "added") {
                 console.log("new data added: ", change.doc.data());
                 const data = change.doc.data();
 
+                const eventDateInSeconds = data.date.seconds;
+                const reminderTimestamp = Luxon.fromSeconds(eventDateInSeconds)
+                  .minus({ days: 1 })
+                  .set({ hour: 9 });
+
+                const reminderTimeInSeconds = reminderTimestamp.toSeconds();
+
+                const dateTimeNowInSeconds = Math.round(
+                  nowTimestamp.getTime() / 1000
+                );
+
+                const secondsTilReminderNotification =
+                  reminderTimeInSeconds - dateTimeNowInSeconds;
+
+                // reminder 1 day before at 9am
+                scheduleNotification(
+                  {
+                    title: "New Talk n Torque Event!",
+                    body: data.name ? data.name : "VROOOOOOM",
+                    sound: true,
+                    priority: Notifications.AndroidNotificationPriority.HIGH,
+                    color: "red",
+                  },
+                  secondsTilReminderNotification
+                );
+
+                //first immediate notification
                 scheduleNotification(
                   {
                     title: "New Talk n Torque Event!",
@@ -55,6 +83,8 @@ const GlobalNotifications = () => {
     return () => {
       // close event listener when navigation from page
       authListener();
+
+      if (eventListener) eventListener();
     };
   }, []);
 
