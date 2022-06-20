85a009b770a650b19f8c27944374f7ee9bdbb1d9
Kash Pitan <kashpitan@googlemail.com>
2022-06-21 00:46:09 +0100

    added cloudfront functions to handle notifications

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,25 +1,44 @@
 import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
+import fetch from "node-fetch";
 
 admin.initializeApp();
-
 const db = admin.firestore();
 
 // trigger when a new event is added
 exports.newEventNotification = functions.firestore
-  .document("event/{id}")
+  .document("events/{id}")
   .onCreate((snap, context) => {
-    functions.logger.info("Test log");
+    const eventData = snap.data();
 
-    functions.logger.info(snap.data());
-    // const tokens = db.doc();
+    let tokens: string[] = [];
+    let messages: { to: string; body: string }[] = [];
 
-    const payload = {
-      notification: {
-        title: "this is a test",
-        body: "testify",
-      },
-      token: "test",
-    };
-    admin.messaging().send(payload);
+    db.collection("users")
+      .get()
+      .then((snapshot) => {
+        snapshot.forEach((doc) => {
+          const userData = doc.data();
+          functions.logger.info(userData);
+
+          if (userData?.pushNotificationToken) {
+            tokens.push(userData.pushNotificationToken);
+            const userPushToken = userData?.pushNotificationToken;
+            messages.push({ to: userPushToken, body: eventData.name });
+          }
+        });
+        return Promise.all(messages);
+      })
+      .then((messages) => {
+        functions.logger.debug(tokens);
+
+        fetch("https://exp.host/--/api/v2/push/send", {
+          method: "POST",
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(messages),
+        });
+      });
   });
