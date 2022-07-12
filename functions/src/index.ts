a6cb94354217d995913cff22b8ebd401b4ea9421
Kash Pitan <kashpitan@googlemail.com>
2022-07-12 17:15:24 +0100

    added scheduled function for latest event notifications

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,7 @@
 import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
 import fetch from "node-fetch";
+import { DateTime, DateTime as Luxon } from "luxon";
 
 admin.initializeApp();
 const db = admin.firestore();
@@ -42,3 +43,53 @@ exports.newEventNotification = functions.firestore
         });
       });
   });
+
+exports.scheduleEventReminder = functions.pubsub
+  .schedule("every day 01:04")
+  .timeZone("Europe/London")
+  .onRun(async (context) => {
+    const today = Luxon.now();
+    const todayTimestamp = new Date(today.toString());
+
+    const eventsSnapshot = await db
+      .collection("events")
+      .where("date", ">", todayTimestamp)
+      .orderBy("date")
+      .get();
+
+    const latestEvent = eventsSnapshot.docs[0].data();
+
+    let tokens: string[] = [];
+    let messages: { to: string; body: string }[] = [];
+    functions.logger.info(latestEvent);
+
+    db.collection("users")
+      .get()
+      .then((snapshot) => {
+        snapshot.forEach((doc) => {
+          const userData = doc.data();
+          // functions.logger.info(userData);
+
+          if (userData?.pushNotificationToken) {
+            tokens.push(userData.pushNotificationToken);
+            const userPushToken = userData?.pushNotificationToken;
+            messages.push({ to: userPushToken, body: latestEvent.name });
+          }
+        });
+        return Promise.all(messages);
+      })
+      .then((messages) => {
+        functions.logger.debug(tokens);
+
+        fetch("https://exp.host/--/api/v2/push/send", {
+          method: "POST",
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(messages),
+        });
+      });
+  });
+
+//extract send messages out into another function
