b4b607e0c179c819ac573b31de87c89b41ea5bed
Kash Pitan <kashpitan@googlemail.com>
2022-07-13 00:11:24 +0100

    updated event reminder function to only send notifications to events a user has signed up to

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -45,9 +45,9 @@ exports.newEventNotification = functions.firestore
   });
 
 exports.scheduleEventReminder = functions.pubsub
-  .schedule("every day 01:04")
+  .schedule("every day 00:04")
   .timeZone("Europe/London")
-  .onRun(async (context) => {
+  .onRun(async () => {
     const today = Luxon.now();
     const todayTimestamp = new Date(today.toString());
 
@@ -58,38 +58,25 @@ exports.scheduleEventReminder = functions.pubsub
       .get();
 
     const latestEvent = eventsSnapshot.docs[0].data();
+    const attendanceList = latestEvent.attendance;
 
-    let tokens: string[] = [];
     let messages: { to: string; body: string }[] = [];
     functions.logger.info(latestEvent);
+    functions.logger.info(attendanceList);
+    Object.keys(attendanceList).forEach((key) => {
+      functions.logger.info(key, attendanceList[key]);
+      const userPushToken = attendanceList[key].pushNotificationToken;
 
-    db.collection("users")
-      .get()
-      .then((snapshot) => {
-        snapshot.forEach((doc) => {
-          const userData = doc.data();
-          // functions.logger.info(userData);
+      if (userPushToken)
+        messages.push({ to: userPushToken, body: latestEvent.name });
+    });
 
-          if (userData?.pushNotificationToken) {
-            tokens.push(userData.pushNotificationToken);
-            const userPushToken = userData?.pushNotificationToken;
-            messages.push({ to: userPushToken, body: latestEvent.name });
-          }
-        });
-        return Promise.all(messages);
-      })
-      .then((messages) => {
-        functions.logger.debug(tokens);
-
-        fetch("https://exp.host/--/api/v2/push/send", {
-          method: "POST",
-          headers: {
-            Accept: "application/json",
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(messages),
-        });
-      });
+    fetch("https://exp.host/--/api/v2/push/send", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(messages),
+    });
   });
-
-//extract send messages out into another function
