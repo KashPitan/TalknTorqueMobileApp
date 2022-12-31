1804876326179d8ed00641387ed0dfe5ffcba080
Kash Pitan <kashpitan@googlemail.com>
2022-12-31 01:44:17 +0000

    feat: updated ui for upcoming event items (temp past events)

--- src/components/UpcomingEventItem.tsx
+++ src/components/UpcomingEventItem.tsx
@@ -1,72 +1,118 @@
-import { useNavigation } from "@react-navigation/native";
-import { Box, HStack, VStack, Text, Center, Button } from "native-base";
-import React, { FC } from "react";
-import { StyleSheet } from "react-native";
-import { EventType } from "../../types";
+import { useNavigation } from '@react-navigation/native';
+import { getDownloadURL, ref } from 'firebase/storage';
+
+import { HStack, Text, Center, Button, Image, VStack, Icon } from 'native-base';
+import React, { FC, useEffect, useState } from 'react';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+
+import { storage } from '../../firebase';
+import { EventType } from '../../types';
+import { DIMENSIONS } from '../constants/constants';
+import { colors } from '../constants/themes';
+import { Pressable } from 'native-base';
 
 const UpcomingEventItem: FC<{ event: EventType }> = ({
   children,
   event,
 }): JSX.Element => {
+  const firebaseStorageReference = ref(storage, event.imageUri);
+  const [downloadUrl, setDownloadUrl] = useState<string | null>(null);
+
+  useEffect(() => {
+    // if the uri string contains this its a storage location reference
+    // so we need to get the download url from it
+    // if not it should already be a download url
+    if (event.imageUri?.includes('talktorque')) {
+      (async () => {
+        const firebaseDownloadUrl = await getDownloadURL(
+          firebaseStorageReference
+        );
+        setDownloadUrl(firebaseDownloadUrl);
+      })();
+    }
+  }, []);
   const navigation = useNavigation();
   return (
-    <Button
-      style={styles.actionSheetItem}
-      bg="gray.300"
+    <Pressable
       p="0"
-      my="1"
-      onPress={() => navigation.navigate("Event Screen", { event })}
-      rounded="lg"
-      // startIcon={}
+      my="2"
+      mx="2"
+      onPress={() => navigation.navigate('Event Screen', { event })}
     >
-      <HStack>
-        <Box
-          bgColor="red.400"
-          w="23%"
-          // h="full"
-          rounded="lg"
-          borderWidth="3"
-          borderColor="red.600"
-          // mr="2"
-          // ml="3"
-        >
-          <Center>
-            <VStack>
-              <Center>
-                <Text bold fontSize="2xl">
-                  {event.day}
-                </Text>
-              </Center>
-
-              <Text bold fontSize="md">
-                {event.month}
+      {({ isPressed }) => {
+        return (
+          <HStack
+            style={{
+              transform: [
+                {
+                  scale: isPressed ? 0.94 : 1,
+                },
+              ],
+            }}
+            rounded="lg"
+            shadow={6}
+            bgColor={isPressed ? 'gray.200' : 'white'}
+          >
+            <Center>
+              <Image
+                source={{ uri: downloadUrl ?? '' }}
+                style={{
+                  width: DIMENSIONS.width / 5 + 10,
+                  height: DIMENSIONS.height / 10,
+                }}
+                rounded="lg"
+                resizeMode="cover"
+              />
+            </Center>
+            <Center w="72%" ml="3">
+              <Text
+                alignSelf="flex-start"
+                bold
+                fontSize="xl"
+                color={colors.text.main}
+              >
+                {event.name}
               </Text>
-            </VStack>
-          </Center>
-        </Box>
-        <Center w="74%" ml="2">
-          <Text alignSelf="flex-start" bold fontSize="xl" color="red.600">
-            {event.name}
-          </Text>
-          <Text alignSelf="flex-start" fontSize="sm">
-            {event.location}
-          </Text>
-          <Text alignSelf="flex-start" isTruncated>
-            {event.fullDate}
-          </Text>
-        </Center>
-      </HStack>
-    </Button>
+
+              <HStack alignSelf="flex-start">
+                <Icon
+                  as={MaterialCommunityIcons}
+                  name="map-marker"
+                  color={colors.text.highlight}
+                  mt="1"
+                  mr="1"
+                />
+                <Text
+                  alignSelf="flex-start"
+                  fontSize="sm"
+                  color={colors.text.light}
+                >
+                  {event.location}
+                </Text>
+              </HStack>
+
+              <HStack alignSelf="flex-start">
+                <Icon
+                  as={MaterialCommunityIcons}
+                  name="calendar"
+                  color={colors.text.highlight}
+                  mt="1"
+                  mr="1"
+                />
+                <Text
+                  alignSelf="flex-start"
+                  color={colors.text.light}
+                  isTruncated
+                >
+                  {event.fullDate}
+                </Text>
+              </HStack>
+            </Center>
+          </HStack>
+        );
+      }}
+    </Pressable>
   );
 };
 
 export default UpcomingEventItem;
-
-const styles = StyleSheet.create({
-  actionSheetItem: {
-    // shadowColor: "#171717",
-    // shadowOffset: { width: -2, height: 4 },
-    // shadowOpacity: 0.2,
-    // shadowRadius: 3,
-  },
-});
