2d705593fa5d746121d5d8080d35547bafe0d9b5
Kash Pitan <kashpitan@googlemail.com>
2022-12-31 01:40:02 +0000

    feat: ui updates for next event component

--- src/components/NextEvent.tsx
+++ src/components/NextEvent.tsx
@@ -1,16 +1,24 @@
 import React, { FC } from "react";
-import { Text, Center } from "native-base";
+import { Text, Center, Skeleton, Box, View } from "native-base";
 import EventCard from "./EventCard";
 import { EventType } from "../../types";
+import { colors } from "../constants/themes";
 
 const NextEvent: FC<{ events: EventType[] }> = ({ events }): JSX.Element => {
   return (
     <>
-      <Text fontSize="lg" color="white" bold ml="7" mb="1">
+      <Text
+        fontSize="lg"
+        color={colors.text.highlight}
+        bold
+        ml="7"
+        mb="2"
+        mt="5"
+      >
         Next event
       </Text>
 
-      <Center alignItems="flex-start" flexDirection="row" px="2" mb="5">
+      <Center alignItems="flex-start" flexDirection="row" px="2">
         {events[0] && <EventCard event={events[0]} />}
       </Center>
     </>
