a8c00ec7a91afaeba8d19696022b614f85ddcecd
Kash Pitan <kashpitan@googlemail.com>
2022-12-31 01:42:55 +0000

    feat: ui updates to image carousel

--- src/components/ImageCarousel.tsx
+++ src/components/ImageCarousel.tsx
@@ -1,33 +1,36 @@
 import { Box, FlatList, Text, Image } from "native-base";
 import React from "react";
-import { StyleSheet } from "react-native";
 import { eventPhotos } from "../../images/TTEventIndex";
+import { colors } from "../constants/themes";
 
 const images = eventPhotos;
 const ImageCarousel = () => {
   return (
-    <Box bgColor="gray.200">
-      <Box my="3" bgColor="red.500" borderTopRadius={40} mb="6" h="full">
-        <Text fontSize="2xl" color="white" bold ml="6" pt="3" mt="4">
-          Gallery
-        </Text>
-        <Box w="11%" h="1" bgColor="white" rounded="xl" mb="3" ml="8" />
+    <Box h="100%">
+      <Text
+        fontSize="lg"
+        color={colors.text.highlight}
+        bold
+        ml="6"
+        mb="3"
+        mt="3"
+      >
+        Gallery
+      </Text>
 
-        <FlatList
-          keyExtractor={(item, index) => index.toString()}
-          data={images}
-          renderItem={(item) => (
-            <Image source={item.item} mx="2" size="2xl" rounded="2xl" />
-          )}
-          horizontal
-          initialScrollIndex={1}
-          mb="6"
-        ></FlatList>
-      </Box>
+      <FlatList
+        keyExtractor={(item, index) => index.toString()}
+        data={images}
+        renderItem={(item) => (
+          <Image source={item.item} mx="2" size="2xl" rounded="2xl" />
+        )}
+        horizontal
+        initialScrollIndex={1}
+        mb="6"
+        px="4"
+      ></FlatList>
     </Box>
   );
 };
 
 export default ImageCarousel;
-
-const styles = StyleSheet.create({});
