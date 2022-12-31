d5831dc9839524dda7e6348e8eef368a742deb78
Kash Pitan <kashpitan@googlemail.com>
2022-12-31 01:38:26 +0000

    refactor: simplified home screen by splitting out into components

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,23 +1,34 @@
-import { Box, Text, ScrollView } from "native-base";
-import React, { useCallback, useEffect, useState } from "react";
-import { RefreshControl } from "react-native";
+import { Box, Text, ScrollView } from 'native-base';
+import React, { useCallback, useEffect, useState } from 'react';
+import { RefreshControl } from 'react-native';
 
-import Header from "../components/Header";
-import UpcomingEvents from "../components/UpcomingEvents";
-import ImageCarousel from "../components/ImageCarousel";
+import { DateTime as Luxon } from 'luxon';
 
-import { EventType } from "../../types";
-import Event from "../classes/Event";
-import NextEvent from "../components/NextEvent";
+import { colors } from '../constants/themes';
+
+import Header from '../components/layout/Header';
+import UpcomingEvents from '../components/UpcomingEvents';
+import ImageCarousel from '../components/ImageCarousel';
+
+import { EventType } from '../../types';
+import Event from '../classes/Event';
+import PastEvents from '../components/PastEvents';
+import NextEvent from '../components/NextEvent';
+import Footer from '../components/layout/Footer';
 
 const HomeScreen = () => {
   const [events, setEvents] = useState<EventType[]>([]);
-  const [refreshing, setRefreshing] = React.useState(false);
+  const [isDataLoaded, setIsDataLoaded] = useState(false);
+  const [refreshing, setRefreshing] = useState(false);
 
   const getScreenData = async () => {
     setRefreshing(true);
+    setIsDataLoaded(false);
+
     const eventData = await Event.getMostRecentEvents();
     setEvents(eventData);
+
+    setIsDataLoaded(true);
     setRefreshing(false);
   };
 
@@ -27,33 +38,39 @@ const HomeScreen = () => {
 
   useEffect(() => {
     (async () => {
-      const eventData = await Event.getMostRecentEvents();
-      setEvents(eventData);
+      await getScreenData();
     })();
   }, []);
 
   return (
     <>
-      <Header />
-
-          {events && (
-            <>
-              <UpcomingEvents events={events} />
-              <ImageCarousel />
-            </>
-          )}
-        </Box>
       <ScrollView
-        bg="red.500"
+        bg={colors.background}
         refreshControl={
           <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
         }
       >
-        <Text fontSize="3xl" color="white" bold ml="7" mb="1">
-          Events
+        <Header />
+
+        <Text fontSize="3xl" color={colors.text.main} bold ml="7" mt="3">
+          Explore Events
         </Text>
+        <Text fontSize="sm" color="gray.400" ml="7">
+          {Luxon.now().toFormat('EEEE, d MMMM')}
+        </Text>
+
         <NextEvent events={events} />
+
+        {events && (
+          <>
+            <UpcomingEvents events={events} />
+            <PastEvents events={events} />
+          </>
+        )}
+
+        <ImageCarousel />
       </ScrollView>
+      <Footer />
     </>
   );
 };
